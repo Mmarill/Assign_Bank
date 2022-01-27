8c208953415ba5a84785c9b1e8fa0f4ffd09832b
Mmarill <Max.henkesson@gmail.com>
2022-01-27 15:28:01 +0100

    Fix objektorient

--- bank.py
+++ bank.py
@@ -2,8 +2,8 @@ import re
 from customer import Customer
 from account import Account
 
-class Bank():
 
+class Bank:
     customers = []
     id = []
     ctxt = "customers.txt"
@@ -16,17 +16,20 @@ class Bank():
         self.name = "MyBank"
         self.customers = []
         self._load()
-        self.get_customers()
+        self.load_customers()
+        self.load_accounts()
+        self.acc_list = []
+        self.get_accounts()
 
     def _load(self):
         self.customer_data = []
 
-        with open (self.ctxt) as txtFile:
+        with open(self.ctxt) as txtFile:
             for x in txtFile:
                 self.customer_data.append(x.strip())
         return self.customer_data
 
-    def get_customers(self):
+    def load_customers(self):
 
         for i in self.customer_data:
             x = re.sub("#", ":", str(i))
@@ -36,10 +39,10 @@ class Bank():
 
         return self.customers
 
-    def get_customers_test(self):
+    def get_customers(self):
 
         for cstmr in self.customers:
-            print(cstmr.name, cstmr.id, cstmr.pnr)
+            print(cstmr.name, cstmr.pnr)
 
     def get_customer(self, pnr):
 
@@ -53,24 +56,43 @@ class Bank():
                 id = y[0]
                 for line in self.accounts:
                     if id in str(line):
-                            self.acc.append(line)
-                            z = re.sub(",", ":", str(self.acc))
-                            t = str(z).strip().split(":")
-                            if len(t) > 3:
-                                firstacc = (f"First account:\n\tAccount no: {t[0]}\n\tAccount type: {t[1]}\n\tBalance: {t[2]}\n")
-                                secondacc = (f"Second account: \n\tAccount no: {t[3]}\n\tAccount type: {t[4]}\n\tBalance: {t[5]}")
-                            elif len(t) == 3:
-                                firstacc = (f"First account:\n\tAccount no: {t[0]}\n\tAccount type: {t[1]}\n\tBalance: {t[2]}\n")
-                                secondacc = (f"Second account: \n\tAccount no: {None}\n\tAccount type: {None}\n\tBalance: {None}")
+                        self.acc.append(line)
+                        z = re.sub(",", ":", str(self.acc))
+                        t = str(z).strip().split(":")
+                        if len(t) > 3:
+                            firstacc = (
+                                f"First account:\n\tAccount no: {t[0]}\n\tAccount type: {t[1]}\n\tBalance: {t[2]}\n")
+                            secondacc = (
+                                f"Second account: \n\tAccount no: {t[3]}\n\tAccount type: {t[4]}\n\tBalance: {t[5]}")
+                        elif len(t) == 3:
+                            firstacc = (
+                                f"First account:\n\tAccount no: {t[0]}\n\tAccount type: {t[1]}\n\tBalance: {t[2]}\n")
+                            secondacc = (
+                                f"Second account: \n\tAccount no: {None}\n\tAccount type: {None}\n\tBalance: {None}")
 
         print(f"Customer id: {y[0]}\nName: {y[1]} {y[2]}\nSocial security number: {y[3]}\n{firstacc}{secondacc}")
 
-    def get_accounts(self):
+    def get_customer(self, pnr):
+
+        for x in self.customers:
+            if pnr == x.pnr:
+
+                if len(t) > 3:
+                    firstacc = (
+                        f"First account:\n\tAccount no: {t[0]}\n\tAccount type: {t[1]}\n\tBalance: {t[2]}\n")
+                    secondacc = (
+                        f"Second account: \n\tAccount no: {t[3]}\n\tAccount type: {t[4]}\n\tBalance: {t[5]}")
+                elif len(t) == 3:
+                    firstacc = (
+                        f"First account:\n\tAccount no: {t[0]}\n\tAccount type: {t[1]}\n\tBalance: {t[2]}\n")
+                    secondacc = (
+                        f"Second account: \n\tAccount no: {None}\n\tAccount type: {None}\n\tBalance: {None}")
+
+    def load_accounts(self):
 
         self.accounts = []
         self.accountNo = []
 
-        Bank._load(self)
         account_data = {}
 
         for i in self.customer_data:
@@ -79,42 +101,41 @@ class Bank():
 
         for x, y in account_data.items():
             if len(y) > 3:
-                first_account = Account(int(x), int(y[0]), y[1], float(y[2].split("#")[0]))
-                second_account = Account(int(x), int(y[2].split("#")[1]), y[3], float(y[4]))
+                first_account = Account(str(x), str(y[0]), y[1], float(y[2].split("#")[0]))
+                second_account = Account(str(x), str(y[2].split("#")[1]), y[3], float(y[4]))
                 self.accounts.append(first_account)
                 self.accounts.append(second_account)
                 self.accountNo.append(y[0])
                 self.accountNo.append(y[2].split("#")[1])
             elif len(y) == 3:
-                first_account = Account(int(x), int(y[0]), y[1], float(y[2]))
+                first_account = Account(str(x), str(y[0]), y[1], float(y[2]))
                 self.accounts.append(first_account)
                 self.accountNo.append(y[0])
             else:
                 pass
 
-    def get_account(self, pnr, id):
-        Bank.get_accounts(self)
-        Bank.get_customers(self)
-        temp = 0
+        return self.accounts
+
+    def get_accounts(self):
+
+        self.acc_list = []
+
+        for i in self.accounts:
+            account = (str(i.accountNo), i.accountType, str(i.balance))
+            self.acc_list.append(account)
+
+    def get_account(self, pnr, acc_no):
+
         if re.match('[0-9]{6}-[0-9]{4}', pnr) is None:
             print("Sorry wrong format, please enter personal number as xxxxxx-xxxx")
         else:
-            for i in self.customers:
-                if pnr in repr(i):
-                    x = str(i).split()
-                    cstm_id = x[0]
-                    for line in self.accounts:
-                        if cstm_id + id in str(line):
-                            linelist = str(line).split(" ")
-                            return f"Account number: {linelist[1]}, account type: {linelist[2]} {linelist[3]}, balance: {linelist[4]}"
-                        else:
-                            temp += 2
-                    if temp == len(self.accounts)*2:
-                        print(f"This customer doesnt have an account with this id: {id}")
-                elif pnr not in repr(i):
-                    temp += 1
-            if temp==len(self.customers):
-                print(f"No customer with either social security number: {pnr} or with account number: {id}")
+            for x in self.customers:
+                if pnr == x.pnr:
+                    for y in self.accounts:
+                        if acc_no == y.accountNo and y.id == x.id:
+                            return f'{y.accountNo}, {y.accountType}, {y.balance}'
+                    return print(f"No account found with account number {acc_no}.")
+            return print(f"No customer found with {pnr} in list")
 
     def add_customer(self, name, pnr):
 
@@ -125,10 +146,11 @@ class Bank():
             print("Customer with same social security number already exists")
             return False
         else:
-            textfile = open("customers.txt","a")
-            textfile.write('\n' + Bank.get_new_id(self) + f':{name}:{pnr}:' + Bank.get_top_account(self) + f':debit account:0.0')
+            textfile = open("customers.txt", "a")
+            textfile.write(
+                '\n' + Bank.get_new_id(self) + f':{name}:{pnr}:' + Bank.get_top_account(self) + f':debit account:0.0')
             textfile.close()
-            return True
+            return print(f'New Customer {name}: {pnr} created')
 
     def change_customer_name(self, newname, pnr):
         Bank._load(self)
@@ -175,12 +197,11 @@ class Bank():
         return str(new_id)
 
     def get_top_account(self):
-        self.accountNo = []
 
-        Bank.get_accounts(self)
         newAccount = int(max(self.accountNo)) + 1
 
         return str(newAccount)
+
 
 m = Bank()
 m.get_customer("401132-0676")
